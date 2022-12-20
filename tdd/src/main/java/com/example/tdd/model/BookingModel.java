58ff287276176cd65f58492f75ad7594fe80ab85
PFAR92 <paulofelipetj@hotmail.com>
2022-12-20 18:43:35 -0300

    TDD usando @mockBean metodos get e post

--- tdd/src/main/java/com/example/tdd/model/BookingModel.java
+++ tdd/src/main/java/com/example/tdd/model/BookingModel.java
@@ -1,7 +1,10 @@
 package com.example.tdd.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
@@ -9,8 +12,11 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @Getter
 @Setter
+@Entity
+@NoArgsConstructor
 public class BookingModel {
 
+    @Id
     private String id;
     private String reserveName;
     private LocalDate checkIn;
--- tdd/src/main/java/com/example/tdd/repository/BookingRepository.java
+++ tdd/src/main/java/com/example/tdd/repository/BookingRepository.java
@@ -2,8 +2,11 @@ package com.example.tdd.repository;
 
 import com.example.tdd.model.BookingModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
+@Repository
 public interface BookingRepository extends JpaRepository<BookingModel, String> {
 
     Optional<BookingModel> findByReserveName(String name);
